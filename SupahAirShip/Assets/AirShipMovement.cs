9f476180da491d53c5d0daf7782f20260d381214
christiancelda <christiancelda@gmail.com>
2014-11-13 18:51:38 +0100

    Script de movimiento con acelerometro y apk's de prueba

---
+++ SupahAirShip/Assets/AirShipMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirShipMovement : MonoBehaviour {
+
+	public float zMovement;
+	Vector3 acceler;
+
+	// Use this for initialization
+	void Start () {
+		acceler = Input.acceleration;
+
+		if (acceler.sqrMagnitude > 1) {
+			acceler.Normalize();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0, 0 , zMovement);
+		transform.Rotate(Input.acceleration.y * 2, Input.acceleration.x * 2, 0);
+	}
+}
